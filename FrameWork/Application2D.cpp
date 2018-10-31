160c0e488728a9900b9f92ceb9576377b6ea8c72
Ram T <rk.amumar@gmail.com>
2018-10-31 15:22:16 +1100

    window creation
    creating a GLFW window

--- FrameWork/Application2D.cpp
+++ FrameWork/Application2D.cpp
@@ -1,12 +1,33 @@
 #include "Application2D.h"
-
+#include <GLFW/glfw3.h>
 
 
 Application2D::Application2D()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
+
+
+bool Application2D::createWindow(int width, int height, const char * title, bool fullscreen)
+{
+	
+	GLFWwindow* window = glfwCreateWindow(width, height, title, (fullscreen ? glfwGetPrimaryMonitor() : nullptr), nullptr);
+	if (window == nullptr)
+	{
+		glfwTerminate();
+		return false;
+	}
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow*, int w, int h) {glViewport(0, 0, w, h); });
+	glfwMakeContextCurrent(window);
+	return true;
+}
+
 
 
 Application2D::~Application2D()
 {
 }
+
--- FrameWork/Application2D.h
+++ FrameWork/Application2D.h
@@ -1,8 +1,19 @@
 #pragma once
+struct GLFWwindow;
 class Application2D
 {
 public:
+	/* GLFW - Initialize and Configure */
 	Application2D();
+
+	/* GLFW - window creation */
+	bool createWindow(int width, int height, const char* title, bool fullscreen = false);
+
 	~Application2D();
+
+protected:
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+	GLFWwindow* m_window;
 };
 
